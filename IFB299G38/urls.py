9711f3d786efc4675c712734b943eefa42da0d1e
rhoolahan8 <rhoolahan8@gmail.com>
2018-04-22 16:29:18 +1000

    creating homepage, added blank page for teachers application

--- IFB299G38/urls.py
+++ IFB299G38/urls.py
@@ -19,6 +19,7 @@ urlpatterns = [
     url(r'^$', app.views.home, name='home'),
     url(r'^contact$', app.views.contact, name='contact'),
     url(r'^about', app.views.about, name='about'),
+    url(r'^teachersapplication', app.views.about, name='teachersapplication'),
     url(r'^login/$',
         django.contrib.auth.views.login,
         {
